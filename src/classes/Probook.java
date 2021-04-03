ef22f710b040feeb09bbcc353eae52ac9c130921
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-03 12:54:44 +0100

    Create Probook.java

---
+++ src/classes/Probook.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Probook extends Laptop {
+String type;
+
+public Probook(String cpu, String marque, int ram, int disque, String cartegraphique, float prix, float autonomie,
+		String type) {
+	super(cpu, marque, ram, disque, cartegraphique, prix, autonomie);
+	this.type = type;
+}
+
+
+
+
+
+}
